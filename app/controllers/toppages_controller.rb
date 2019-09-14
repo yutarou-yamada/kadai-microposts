2eb2f8f9eb0263233925448b18856c347dad754a
yutarou.yamada <pennyrain551@gmail.com>
2019-09-14 05:24:18 +0000

    7th commit

---
+++ app/controllers/toppages_controller.rb
@@ -0,0 +1,8 @@
+class ToppagesController < ApplicationController
+  def index
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
+  end
+end
+end
---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+
+  validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :favorites, dependent: :destroy
+end
